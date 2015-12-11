b8799c9a321339ef77e57fbbe4928522e414b1f5
Silvan Adrian <hallo@silvanadrian.ch>
2015-12-11 14:45:48 +0100

    get ServiceModules

--- app/app.js
+++ app/app.js
@@ -1,10 +1,17 @@
 angular.module('presentation', [])
     .controller('presentationController', function($scope, $http){
         $scope.services = [];
+        $scope.servicemodules = [];
 
         $scope.findServices = function () {
             $http.get("http://app.silvn.com:8080/api/services").success(function (data) {
                 $scope.services = data;
+            })
+        };
+
+        $scope.findServiceModules = function () {
+            $http.get("http://app.silvn.com:8080/api/servicemodules").success(function (data) {
+                $scope.servicemodules = data;
             })
         };
 
