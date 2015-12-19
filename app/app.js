59596fca405f4e4b2d98a39da56b96a2a1443805
Silvan Adrian <hallo@silvanadrian.ch>
2015-12-19 20:43:32 +0100

    Restful API + some other stuff

--- app/app.js
+++ app/app.js
@@ -1,34 +1,118 @@
 var pres = angular.module('presentation', [])
     .controller('presentationController', function($scope, $http){
+
+      //Services
         $scope.services = [];
         $scope.service = [];
-        $scope.servicemodules = [];
+        $scope.deleteservice = [];
 
-        $scope.putservice =[
-            {"id":1,"serviceName":"Network","modules":[{"id":1,"category":"Network","size":null,"provider":"LibVirt",
-            "config":"<network>\n\n\t<name>network-{{UUID}}</name>\n\t<bridge />\n\t<forward mode=\"nat\" />\n\t\n\t<ip address=\"192.168.122.1\" netmask=\"255.255.255.0\">\n\t\t<dhcp>\n\t\t\t<range start=\"192.168.122.2\" end=\"192.168.122.254\" />\n\t\t</dhcp>\n\t</ip>\n\t\n\t<ip family=\"ipv6\" address=\"2001:db8:ca2:2::1\" prefix=\"64\" >\n\t\t<dhcp>\n\t\t\t<range start=\"2001:db8:ca2:2:1::10\" end=\"2001:db8:ca2:2:1::ff\" />\n\t\t</dhcp>\n\t</ip>\n\t\n</network>",
-            "name":"Network Example"}]}
-        ];
+
+        $scope.putservice =
+            {"id":1,"serviceName":"Updated Compute",
+            "modules":[{"id":1,"category":"Compute",
+            "size":"M","provider":"LibVirt","config":"config","name":"Compute"}]};
+        $scope.postservice =
+            {"serviceName":"New Compute",
+            "modules":[{"id":1,"category":"Compute",
+            "size":"M","provider":"LibVirt","config":"config","name":"Compute"}]};
+            $scope.deleteservice =
+                {"id":4,"serviceName":"New Compute",
+                "modules":[{"id":1,"category":"Compute",
+                "size":"M","provider":"LibVirt","config":"config","name":"Compute"}]};
+
+        $scope.orderservice =   {"id":2,"serviceName":"Network","modules":[{"id":2,"category":"Network",
+        "size":null,"provider":"LibVirt","config":"<network>\n\t<name>network-{{UUID}}</name>\n\t</network>\n",
+        "name":"Network"}]};
 
         $scope.findServices = function () {
-            $http.get("http://app.silvn.com:8080/api/services").success(function (data) {
-                $scope.service = data;
-            })
-        };
-
-        $scope.findService = function () {
-            $http.get("http://app.silvn.com:8080/api/services/1").success(function (data) {
+            $http.get("http://localhost:8080/api/services").success(function (data) {
                 $scope.services = data;
             })
         };
 
-        $scope.updateService = function(service) {
-          Service.update({ id:$routeParams.serviceId }, service);
+        $scope.findService = function () {
+            $http.get("http://localhost:8080/api/services/1").success(function (data) {
+                $scope.service = data;
+            })
         };
 
+        $scope.updateService = function() {
+            $http.put("http://localhost:8080/api/services/1", $scope.putservice).success(function() {
+                }).error(function() {
+                  console.log("error");
+                  });
+        };
+
+        $scope.createService = function() {
+            $http.post("http://localhost:8080/api/services/new", $scope.postservice).success(function() {
+                }).error(function() {
+                  console.log("error");
+                  });
+        };
+
+        $scope.deleteService = function() {
+            $http.delete("http://localhost:8080/api/services/4", 4).success(function() {
+                }).error(function() {
+                  console.log("error");
+                  });
+        };
+
+        $scope.orderService = function() {
+            $http.post("http://localhost:8080/api/services/2", $scope.orderservice).success(function() {
+                }).error(function() {
+                  console.log("error");
+                  });
+        };
+
+
+
+        //OrderedServices
+        $scope.orderedservices = [];
+        $scope.orderedservice = [];
+        $scope.findOrderedServices = function () {
+            $http.get("http://localhost:8080/api/orderedservices").success(function (data) {
+                $scope.orderedservices = data;
+            })
+        };
+
+        $scope.findOrderedService = function () {
+            $http.get("http://localhost:8080/api/orderedservices/1").success(function (data) {
+                $scope.orderedservice = data;
+            })
+        };
+
+        $scope.cancelOrderedService = function () {
+            $http.delete("http://localhost:8080/api/orderedservices/1" ,1).success(function (data) {
+
+            })
+        };
+
+        //ServiceModule
+        $scope.servicemodules = [];
+        $scope.categories;
+        $scope.sizes;
+        $scope.providers;
         $scope.findServiceModules = function () {
-            $http.get("http://app.silvn.com:8080/api/servicemodules").success(function (data) {
+            $http.get("http://localhost:8080/api/servicemodules").success(function (data) {
                 $scope.servicemodules = data;
+            })
+        };
+
+        $scope.findCategories = function () {
+            $http.get("http://localhost:8080/api/servicemodules/categories").success(function (data) {
+                $scope.categories = data;
+            })
+        };
+
+        $scope.findSizes = function () {
+            $http.get("http://localhost:8080/api/servicemodules/sizes").success(function (data) {
+                $scope.sizes = data;
+            })
+        };
+
+        $scope.findProviders = function () {
+            $http.get("http://localhost:8080/api/servicemodules/providers").success(function (data) {
+                $scope.providers = data;
             })
         };
 
@@ -36,15 +120,15 @@ var pres = angular.module('presentation', [])
 
 
 
-pres.factory('ServiceModule', ['$resource', function($resource) {
-    return $resource('http://app.silvn.com:8080/api/servicemodules/:id', null,
-        {
-            'update': { method:'PUT' }
-        });
-}]);
-pres.factory('Service', ['$resource', function($resource) {
-    return $resource('http://app.silvn.com:8080/api/services/:id', {id: '@id'},
-        {
-            'update': { method:'PUT' }
-        });
-}]);
+    pres.factory('ServiceModule', ['$resource', function($resource) {
+        return $resource('http://localhost:8080/api/servicemodules/:id', null,
+            {
+                'update': { method:'PUT' }
+            });
+    }]);
+    pres.factory('Service', ['$resource', function($resource) {
+        return $resource('http://localhost:8080/api/services/:id', {id: '@id'},
+            {
+                'update': { method:'PUT' }
+            });
+    }]);
